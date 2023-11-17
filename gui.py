0435297e0cb7fa8fb69bd83e4621aedf3767c245
Francisco Axel Roman Cardoza <franaxel@live.com.mx>
2023-11-17 13:20:57 -0600

    Correccion del metodo traducir_a_ingles

--- gui.py
+++ gui.py
@@ -57,30 +57,30 @@ def analizar_datos():
         mensajes = file.readlines()
 
 # Función para traducir un mensaje a inglés
-def traducir_a_ingles(mensaje):
-    translator = Translator()
-    try:
-        translation = translator.translate(mensaje, src=detect(mensaje), dest='en')
-        return translation.text
-    except:
-        return mensaje
+    def traducir_a_ingles(mensaje):
+        translator = Translator()
+        try:
+            translation = translator.translate(mensaje, src=detect(mensaje), dest='en')
+            return translation.text
+        except:
+            return mensaje
 
-# Traducir todos los mensajes a inglés
-mensajes_traducidos = [traducir_a_ingles(mensaje) for mensaje in mensajes]
+    # Traducir todos los mensajes a inglés
+    mensajes_traducidos = [traducir_a_ingles(mensaje) for mensaje in mensajes]
 
-# Crear un DataFrame con los mensajes traducidos
-datos = pd.DataFrame({'Texto': mensajes_traducidos})
+    # Crear un DataFrame con los mensajes traducidos
+    datos = pd.DataFrame({'Texto': mensajes_traducidos})
 
-# Análisis de sentimientos
-datos['Sentimiento'] = datos['Texto'].apply(lambda x: TextBlob(str(x)).sentiment.polarity)
+    # Análisis de sentimientos
+    datos['Sentimiento'] = datos['Texto'].apply(lambda x: TextBlob(str(x)).sentiment.polarity)
 
-# Visualizar distribución de sentimientos
-plt.figure(figsize=(10, 6))
-plt.hist(datos['Sentimiento'], bins=30, edgecolor='black')
-plt.title('Distribución de Sentimientos')
-plt.xlabel('Sentimiento')
-plt.ylabel('Frecuencia')
-plt.show()
+    # Visualizar distribución de sentimientos
+    plt.figure(figsize=(10, 6))
+    plt.hist(datos['Sentimiento'], bins=30, edgecolor='black')
+    plt.title('Distribución de Sentimientos')
+    plt.xlabel('Sentimiento')
+    plt.ylabel('Frecuencia')
+    plt.show()
 
 """Analisis de datos Linkedin"""
 # Cargar el modelo de procesamiento de lenguaje natural de spaCy
