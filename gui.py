aa40a6244c8a8ff41453da61445e7a9e44f0e7cd
Francisco Axel Roman Cardoza <franaxel@live.com.mx>
2023-11-17 11:38:04 -0600

    Se agregaron los algoritmos de analisis de datos para las redes sociales LinkedIn, GitHub y Twitter

--- gui.py
+++ gui.py
@@ -1,6 +1,19 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
+#Twitter
+import pandas as pd
+import matplotlib.pyplot as plt
+from textblob import TextBlob
+from wordcloud import WordCloud
+from langdetect import detect
+from googletrans import Translator
+#linkedin
+import spacy
+#github
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 def cargar_cv():
     cv_filename = filedialog.askopenfilename(filetypes=[("Archivos PDF", "*.pdf"), ("Todos los archivos", "*.*")])
@@ -39,7 +52,112 @@ def extraer_datos_redes():
 
 def analizar_datos():
     # Puedes colocar aquí la lógica para analizar los datos
-    messagebox.showinfo("Análisis de Datos", "Datos analizados")
+    """Analisis de datos Twitter"""
+    with open('datos_redes_sociales.txt', 'r', encoding='utf-8') as file:
+        mensajes = file.readlines()
+
+# Función para traducir un mensaje a inglés
+def traducir_a_ingles(mensaje):
+    translator = Translator()
+    try:
+        translation = translator.translate(mensaje, src=detect(mensaje), dest='en')
+        return translation.text
+    except:
+        return mensaje
+
+# Traducir todos los mensajes a inglés
+mensajes_traducidos = [traducir_a_ingles(mensaje) for mensaje in mensajes]
+
+# Crear un DataFrame con los mensajes traducidos
+datos = pd.DataFrame({'Texto': mensajes_traducidos})
+
+# Análisis de sentimientos
+datos['Sentimiento'] = datos['Texto'].apply(lambda x: TextBlob(str(x)).sentiment.polarity)
+
+# Visualizar distribución de sentimientos
+plt.figure(figsize=(10, 6))
+plt.hist(datos['Sentimiento'], bins=30, edgecolor='black')
+plt.title('Distribución de Sentimientos')
+plt.xlabel('Sentimiento')
+plt.ylabel('Frecuencia')
+plt.show()
+
+"""Analisis de datos Linkedin"""
+# Cargar el modelo de procesamiento de lenguaje natural de spaCy
+nlp = spacy.load("es_core_news_sm")  # Puedes usar un modelo específico para tu idioma
+ 
+# Texto de ejemplo: Descripción del puesto y experiencia del candidato
+descripcion_puesto = "Buscamos un desarrollador de Python con experiencia en desarrollo web y al menos 2 años de experiencia."
+experiencia_candidato = "Desarrollador de Python con 3 años de experiencia en desarrollo web."
+ 
+# Procesar la descripción del puesto y la experiencia del candidato
+doc_puesto = nlp(descripcion_puesto)
+doc_candidato = nlp(experiencia_candidato)
+ 
+# Función para evaluar la similitud contextual
+def evaluar_similitud_contextual(doc1, doc2):
+	similitud = doc1.similarity(doc2)
+	return similitud
+ 
+# Evaluar la similitud contextual entre la descripción del puesto y la experiencia del candidato
+similitud_contextual = evaluar_similitud_contextual(doc_puesto, doc_candidato)
+ 
+# Establecer un umbral de similitud
+umbral_similitud = 0.7  # Puedes ajustar este umbral según tus criterios
+ 
+# Evaluar si la experiencia del candidato es coherente con la descripción del puesto
+if similitud_contextual >= umbral_similitud:
+	print("La experiencia del candidato es coherente con la descripción del puesto.")
+else:
+	print("La experiencia del candidato no es coherente con la descripción del puesto.")
+ 
+# Imprimir la similitud contextual (puede ser útil para fines de análisis)
+print(f"Similitud Contextual: {similitud_contextual:.2f}")
+
+
+"""Analisis de datos GitHub"""
+# Datos de ejemplo: candidatos y requisitos del trabajo
+candidatos = pd.DataFrame({
+    'ID': [1, 2, 3],
+    'Experiencia': [
+        "Desarrollador de Python con 3 años de experiencia en desarrollo web.",
+        "Ingeniero de software con experiencia en Java y C++.",
+        "Programador junior con conocimientos de Python y JavaScript."
+    ]
+})
+
+requisitos_puesto = "Buscamos un desarrollador de Python con experiencia en desarrollo web y al menos 2 años de experiencia."
+
+# Preprocesamiento de texto
+tfidf_vectorizer = TfidfVectorizer()
+documentos = candidatos['Experiencia'].tolist() + [requisitos_puesto]
+matriz_tfidf = tfidf_vectorizer.fit_transform(documentos)
+
+# Calcular similitud de coseno entre la descripción del trabajo y las experiencias de los candidatos
+similitudes = cosine_similarity(matriz_tfidf[:-1], matriz_tfidf[-1:])
+
+# Calcular una puntuación de relevancia para cada candidato
+puntuaciones_relevancia = similitudes.flatten()
+
+# Asegurarse de que la longitud de puntuaciones_relevancia coincida con la longitud de candidatos
+candidatos['Puntuacion_Relevancia'] = puntuaciones_relevancia[:len(candidatos)]
+
+# Imprimir los resultados
+print(candidatos[['ID', 'Experiencia', 'Puntuacion_Relevancia']])
+
+
+messagebox.showinfo("Análisis de Datos", "Datos analizados")
+
+
+
+
+
+
+
+
+
+
+
 
 # Crear la ventana principal
 ventana = tk.Tk()
